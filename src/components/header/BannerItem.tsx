1bf19706b9563438b163038417f3aef3f380cef6
bipinemp <bhandaribipin3.39@gmail.com>
2023-09-01 19:09:51 +0545

    changes

--- src/components/header/BannerItem.tsx
+++ src/components/header/BannerItem.tsx
@@ -10,18 +10,17 @@ export default function BannerItem({
   bannerUrl: string | StaticImageData;
 }) {
   return (
-    <div className="relative w-full h-[60vh] rounded-lg mb-5 bg-gradient-to-r from-black">
+    <div className="relative w-full h-[60vh] rounded-lg mb-5 mt-2 bg-gradient-to-r from-black">
       <div className="z-10 absolute p-10 w-[50%] h-full flex flex-col gap-3 justify-center">
         <h1 className="font-black text-5xl tracking-wide">
           {selectedBanner.title}
         </h1>
         <p className="text-light text-sm">
-          {selectedBanner.overview.substring(0, 200)} &nbsp;
-          <span className="font-bold text-xl">. . .</span>
+          {selectedBanner.overview.substring(0, 200)} ...
         </p>
         <button
           type="button"
-          className="bg-darkprimary py-2 px-10 rounded-md w-fit font-bold"
+          className="bg-darkprimary py-2 px-10 rounded-full shadow-sm tracking-wide hover:scale-95 hover:opacity-90 transition-all ease-in-out shadow-darkprimary w-fit font-bold"
         >
           View
         </button>
