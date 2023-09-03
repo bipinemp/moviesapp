73cbfc2e91d5262eb271915a9f1b1ee9f9b62033
bipinemp <bhandaribipin3.39@gmail.com>
2023-09-03 09:02:33 +0545

    changes

--- src/components/loading/BannerLoading.tsx
+++ src/components/loading/BannerLoading.tsx
@@ -3,7 +3,7 @@ import Container from "../containers/Container";
 export default function BannerLoading() {
   return (
     <Container>
-      <div className="relative w-full h-[50vh] transition-all rounded-lg bg-indigo-200 animate-pulse"></div>
+      <div className="relative w-full h-[50vh] transition-all rounded-l bg-gray-500 animate-pulse"></div>
     </Container>
   );
 }
--- src/components/loading/PosterLoading.tsx
+++ src/components/loading/PosterLoading.tsx
@@ -1,5 +1,5 @@
 export default function PosterLoading() {
   return (
-    <div className="relative w-[150px] h-[250px] rounded-lg bg-indigo-200 animate-pulse"></div>
+    <div className="relative w-[150px] h-[250px] rounded-lg bg-gray-500 animate-pulse"></div>
   );
 }
