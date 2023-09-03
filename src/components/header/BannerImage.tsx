de2038f990d1a2063013e4f41a7566ce09e262f1
bipinemp <bhandaribipin3.39@gmail.com>
2023-09-03 09:03:57 +0545

    changes

--- src/components/header/BannerImage.tsx
+++ src/components/header/BannerImage.tsx
@@ -10,13 +10,15 @@ export default function BannerImage({
       src={bannerUrl}
       alt="Banner_poster"
       fill
-      className="transition-opacity opacity-0 duration-[500ms] object-fill rounded-lg"
+      className="object-fill rounded-lg"
       priority={true}
       sizes="(min-width: 2020px) 1504px, (min-width: 1540px) calc(82.61vw - 148px), (min-width: 1280px) calc(100vw - 224px), (min-width: 780px) calc(100vw - 80px), calc(100vw - 32px)"
-      onLoadingComplete={(image) => {
-        image.classList.remove("opacity-0");
-        image.classList.add("opacity-20");
-      }}
     />
   );
 }
+
+// className = transition-opacity opacity-0 duration-[500ms]
+// onLoadingComplete={(image) => {
+//   image.classList.remove("opacity-0");
+//   image.classList.add("opacity-20");
+// }}
