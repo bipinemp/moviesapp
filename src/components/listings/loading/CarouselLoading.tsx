c342355b2bb7fb2cca5b05a178151c6ec5ec107f
bipinemp <bhandaribipin3.39@gmail.com>
2023-09-04 15:27:18 +0545

    changes

--- src/components/listings/loading/CarouselLoading.tsx
+++ src/components/listings/loading/CarouselLoading.tsx
@@ -3,13 +3,13 @@ import React from "react";
 function CarouselLoading() {
   return (
     <div className="flex gap-2 overflow-x-auto no-scrollbar space-x-1">
-      <div className="bg-gray-600  animate-pulse w-[280px] h-[200px] rounded-lg"></div>
-      <div className="bg-gray-600  animate-pulse w-[280px] h-[200px] rounded-lg"></div>
-      <div className="bg-gray-600  animate-pulse w-[280px] h-[200px] rounded-lg"></div>
-      <div className="bg-gray-600  animate-pulse w-[280px] h-[200px] rounded-lg"></div>
-      <div className="bg-gray-600  animate-pulse w-[280px] h-[200px] rounded-lg"></div>
-      <div className="bg-gray-600  animate-pulse w-[280px] h-[200px] rounded-lg"></div>
-      <div className="bg-gray-600  animate-pulse w-[280px] h-[200px] rounded-lg"></div>
+      <div className="bg-gray-600  animate-pulse w-[280px] h-[230px] rounded-lg"></div>
+      <div className="bg-gray-600  animate-pulse w-[280px] h-[230px] rounded-lg"></div>
+      <div className="bg-gray-600  animate-pulse w-[280px] h-[230px] rounded-lg"></div>
+      <div className="bg-gray-600  animate-pulse w-[280px] h-[230px] rounded-lg"></div>
+      <div className="bg-gray-600  animate-pulse w-[280px] h-[230px] rounded-lg"></div>
+      <div className="bg-gray-600  animate-pulse w-[280px] h-[230px] rounded-lg"></div>
+      <div className="bg-gray-600  animate-pulse w-[280px] h-[230px] rounded-lg"></div>
     </div>
   );
 }
