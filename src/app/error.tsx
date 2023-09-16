aeae9b5909015ecf672944df135273ce62f8fe99
bipinemp <bhandaribipin3.39@gmail.com>
2023-09-16 23:11:00 +0545

    changes

---
+++ src/app/error.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Container from "@/components/containers/Container";
+import Link from "next/link";
+
+export default function Error() {
+  return (
+    <Container>
+      <div className="flex flex-col mx-auto p-5 rounded-lg gap-3 mt-20 items-center justify-center border-[2px] border-red-600 w-fit">
+        <h2 className="text-xl sm:text-3xl font-bold">Something went wrong!</h2>
+        <h1 className="text-xl sm:text-3xl font-bold text-red-600">:(</h1>
+        <Link
+          href={"/"}
+          className="bg-red-600 py-3 px-5 rounded-2xl font-bold tracking-wide"
+        >
+          Return Home
+        </Link>
+      </div>
+    </Container>
+  );
+}
